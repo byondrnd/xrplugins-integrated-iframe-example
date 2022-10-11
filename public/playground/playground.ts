e5d5e6730f148590adc445913f2907596ea29928
hiren-addweb <hiren@addwebsolution.in>
2022-10-11 16:03:03 +0530

    chnages related to playground and iframe

--- public/playground/playground.ts
+++ public/playground/playground.ts
@@ -10,64 +10,38 @@ import {
 
 const subject = new Subject();
 
+// Gather plugin configuration
 const jsonModule = await import("./config.json", {
   assert: { type: "json" },
 });
 
+// Get available event types
 type XrEventType = keyof typeof XrTypes;
 
 const data: configurations = jsonModule.default;
 
-const emitEvent = (XREvent: XREvent) => {
-  if (Object.keys(XrTypes).includes(XREvent.event)) {
-    eventLogger(XREvent);
-    subject.next(XREvent);
-  } else {
-    console.error("wrong event passed on emit.");
-  }
-};
-
-subject.subscribe((data: any) => {
-  listenToEvent(data.event, data?.callBack);
-});
-
-const listenToEvent = (
-  XREvent: XrEventType,
-  callback?: (XREvent: any) => void
-) => {
-  if (Object.keys(XrTypes).includes(XREvent)) {
-    callbackHandler(XREvent, callback);
-  } else {
-    console.error("unsupported event.");
-  }
-};
-
-const callbackHandler = (event: XrEventType, callback?: Function) => {
-  if (Object.keys(XrTypes).includes(event)) {
-    callback?.(event);
-  }
-};
-
+// Set Iframe and initial plugin.html source
 const iframe =
   (document.querySelector(".inte-iframe") as HTMLIFrameElement) || null;
-
 iframe.src = "../plugin/plugin.html";
-
 const iframeInput = document.querySelector(
   "#inte-input-frame"
 ) as HTMLInputElement;
-
 iframeInput.value = "../plugin/plugin.html";
-
 iframeInput.innerText = "../plugin/plugin.html";
-
 const initBtn = document.querySelector(".inte-init-btn") as HTMLButtonElement;
 
+
+// Initialize iframe source and trigger init event inside it
 const initCalled = () => {
   const iWindow = (iframe?.contentWindow as CustomWindow) || null;
   if (iWindow) {
+
+    // Inject emit and listen functions
     iWindow.emitEvent = emitEvent;
     iWindow.listenToEvent = listenToEvent;
+
+    // Trigger init event inside the iframe
     iWindow.postMessage(
       {
         data,
@@ -76,6 +50,7 @@ const initCalled = () => {
     );
   }
 
+  // Log the successful event generation message
   eventLogger({
     event: "init",
     data: null,
@@ -85,6 +60,7 @@ const initCalled = () => {
   iframe.removeEventListener("click", initCalled);
 };
 
+// Change the iframe source
 const changeIframe = () => {
   const iframeValue = document.getElementById(
     "inte-input-frame"
@@ -98,6 +74,7 @@ const changeIframe = () => {
 
 initBtn.addEventListener("click", changeIframe);
 
+// Log the messsages inside the playground
 const eventLogger = (e: any) => {
   const eventLoggerDiv = document.querySelector(
     ".inte-event-logger-div"
@@ -118,6 +95,7 @@ const generateBtn = document.querySelector(
   ".inte-generate-button"
 ) as HTMLButtonElement;
 
+// Custom event generator function which emits event inside the iframe
 const generateEvent = () => {
   const type = document.querySelector(".inte-input") as HTMLInputElement;
   const typeValue = type?.value;
@@ -130,6 +108,10 @@ const generateEvent = () => {
     from: "playground",
   };
 
+  if (data.event) {
+    subject.next(data);
+  }
+
   iframe.contentWindow?.postMessage(
     {
       data,
@@ -139,3 +121,39 @@ const generateEvent = () => {
 };
 
 generateBtn.onclick = generateEvent;
+
+// Emits event from iframe to playground and pass the XREvent object
+const emitEvent = (XREvent: XREvent) => {
+  if (Object.keys(XrTypes).includes(XREvent.event)) {
+    eventLogger(XREvent);
+    subject.next(XREvent);
+  } else {
+    console.error("wrong event passed on emit.");
+  }
+};
+
+// Subscribe and listen to all the events coming up from the iframe
+subject.subscribe((data: any) => {
+  listenToEvent(data.event, data?.callBack);
+});
+
+
+// Listen event callback handler
+const listenToEvent = (
+  XREvent: XrEventType,
+  callback?: (XREvent: any) => void
+) => {
+  if (Object.keys(XrTypes).includes(XREvent)) {
+    alert(XREvent);
+    callbackHandler(XREvent, callback);
+  } else {
+    console.error("unsupported event.");
+  }
+};
+
+// Iframe callback handler
+const callbackHandler = (event: XrEventType, callback?: Function) => {
+  if (Object.keys(XrTypes).includes(event)) {
+    callback?.(event);
+  }
+};
--- public/plugin/plugin.js
+++ public/plugin/plugin.js
@@ -4,20 +4,21 @@ const config = pluginData.config;
 
 const theming = pluginData.theming;
 
+//postMessage listener to listen init event and alert generated events.
 window.addEventListener("message", function (message) {
   const data = message.data.data;
   if (data.type === "init") {
     const addToCart = document.querySelector(".add-to-cart");
     addToCart.innerHTML = config.button;
     addToCart.style.background = theming.primaryColor;
-  } else {
-    alert(data.event);
   }
 });
 
 function showData() {
   alert("Callback function successfully triggered");
 }
+
+// This function will be called on trigger event click
 function clicked() {
   const data = {
     event: "xr_add_to_cart",
@@ -30,8 +31,11 @@ function clicked() {
     callBack: showData,
   };
 
+  // Emitting xr_add_to_cart with data
   emitEvent(data);
 }
 
 const triggerBtn = document.querySelector(".add-to-cart");
+
+// Click event listener for trigger button inside iframe
 triggerBtn?.addEventListener("click", clicked);
