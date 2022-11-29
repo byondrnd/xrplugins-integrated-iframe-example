dfd55c1bbd21deb9b9c70bf79838967727165b51
hiren-addweb <hiren@addwebsolution.in>
2022-11-29 19:26:17 +0530

    listen event changes.

--- public/plugin/plugin.js
+++ public/plugin/plugin.js
@@ -1,23 +1,33 @@
-//postMessage listener to listen init event and alert generated events.
+//postMessage listener to listen init event.
+
 window.addEventListener("message", function (message) {
   const data = message?.data?.data || message?.data?.dataObj;
+  // code for init exmaple
   if (data?.type === "init" || data?.keyType === "init") {
     const addToCart = document.querySelector(".add-to-cart");
     addToCart.innerHTML = data?.config?.button;
     addToCart.style.background = data?.theming?.primaryColor;
-  } else {
-    callBack(message.data);
+    listenToEvent();
   }
 });
 
-const emitEvent = (XREvent) => {
-  parent.postMessage(XREvent, "*");
-};
+// This callBack is called when event is listen by listenToEvent method
 
 const callBack = (data) => {
   alert("callBack called plugin");
 };
 
+// listen all the emitted events from playground
+
+const listenToEvent = () => {
+  window.addEventListener("message", function (message) {
+    callBack(message.data);
+  });
+};
+
+const emitEvent = (XREvent) => {
+  parent.postMessage(XREvent, "*");
+};
 // This function will be called on trigger event click
 function clicked() {
   const data = {
